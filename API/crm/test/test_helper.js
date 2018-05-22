b2568e7efe6b661a0ba5469e968cde260c76b5f2
unknown <himanshu.44ec06@gmail.com>
2018-05-22 23:20:14 +0530

    delete all entries from user

--- API/crm/test/test_helper.js
+++ API/crm/test/test_helper.js
@@ -5,3 +5,14 @@ mongoose.connect(config.Mongodb);
 mongoose.connection
         .once('open',()=>{ console.log('Good to go !!!') })
        .on('error',(error)=>{ console.error(error); });
+
+beforeEach((done)=>{
+
+    const { users}   = mongoose.connection.collections;
+
+      if(users){
+          users.drop(()=>{
+              done();
+          })
+      }
+})
